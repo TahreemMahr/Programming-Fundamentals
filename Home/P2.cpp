f89d5f87a79a9c07798aa0891b822279d9d7f1af
TahreemMahr <tahreemmehr6@gmail.com>
2026-03-16 19:30:27 +0500

    initial commit of Week 4 files

---
+++ Home/P2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Enter speed: ";
+    int speed;
+    cin >> speed;
+    if (speed > 100)
+    {
+        "cout<<Halt... YOU WILL BE CHALLANGED!!!";
+    }
+    else
+    {
+        cout << "Perfect.. You are going good.";
+    }
+}
---
+++ Lab/Ch2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int salary = 10000;
+    ;
+    int laptop_price = 50000;
+    float advance;
+    int months;
+    advance = salary * 0.5;
+    months = laptop_price / advance;
+    cout << "Months required to buy laptop:" << months;
+}
