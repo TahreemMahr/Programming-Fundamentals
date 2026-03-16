b518e310d80b4b4c4171da25b42329a0be6dfe78
TahreemMahr <tahreemmehr6@gmail.com>
2026-03-16 21:30:53 +0500

    first comit

---
+++ Lab.Week4/C2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int number1, number2;
+    cout << "Enter the first number: ";
+    cin >> number1;
+    cout << "Enter the second number:";
+    cin >> number2;
+    if (number1 < number2)
+    {
+        cout << "Number " << number2 << "  is greater than " << number1;
+    }
+    else
+    {
+        cout << " Number " << " is greater than " << number2;
+    }
+}
---
+++ Lab.Week4/Ch5.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name1, name2, name3;
+    int age1, age2, age3;
+
+    cout << "Enter name of first brother: ";
+    cin >> name1;
+    cout << "Enter age of first brother: ";
+    cin >> age1;
+
+    cout << "Enter name of second brother: ";
+    cin >> name2;
+    cout << "Enter age of second brother: ";
+    cin >> age2;
+
+    cout << "Enter name of third brother: ";
+    cin >> name3;
+    cout << "Enter age of third brother: ";
+    cin >> age3;
+
+    if (age1 < age2)
+    {
+        if (age1 < age3)
+        {
+            cout << name1 << " is youngest";
+        }
+    }
+
+    else
+    {
+        if (age2 < age3)
+        {
+            cout << name2 << " is youngest";
+        }
+        else
+        {
+            cout << name3 << " is youngest";
+        }
+    }
+}
